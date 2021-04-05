c8cf332c7167f7f81628bf0a5dd3bccbdb8d7097
Denton Gentry <dgentry@decarbon.earth>
2021-04-04 21:33:42 -0700

    Remvoe sleeps from start.sh
    Loop tailscale command until it succeeds.
    It blocks until the link is up, then we
    proceed to start the smarthome app.

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@ hostname google-assistant-mqtt
 export TZ='PST8PDT,M3.2.0/2:00:00,M11.1.0/2:00:00'
 
 /app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
-sleep 2
-/app/tailscale up --authkey=${TAILSCALE_AUTHKEY} &
-sleep 2
-
+until /app/tailscale up --authkey=${TAILSCALE_AUTHKEY}
+do
+    sleep 0.1
+done
 all_proxy=socks5://localhost:1055/ /app/smarthome
